44aa9d8a2de12b7b656691ed88e359a89d6050bb
elIgnorante <alvarojzg2@gmail.com>
2025-03-08 12:47:51 -0600

    Configuración inicial

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native-reanimated/lib/typescript/Animated";
+
+const Home = () => {
+  return (
+    <View> 
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+
+export default Home;
